368eb369510de7fdcb3a7e2151fea7899c3dcad1
Taz5150 <70781153+Taz5150@users.noreply.github.com>
2021-03-09 17:49:11 +0100

    Improved Engine Spool-up

--- Support Material/4. Other Scripts/TO_Roll.py
+++ Support Material/4. Other Scripts/TO_Roll.py
@@ -6,6 +6,7 @@ aq = AircraftRequests(sm, _time=0)
 ae = AircraftEvents(sm)
 n1 = 0
 
+
 start_time = time.time()
 aq.set("GENERAL_ENG_THROTTLE_LEVER_POSITION:1", 39.2)
 time.sleep(0.1)
@@ -18,14 +19,14 @@ while n1 < 49.9:
 end_time = time.time() - start_time
 print("IDLE-50%: " + str(end_time))
 time.sleep(2)
-
+#exit()
 start_time = time.time()
 aq.set("GENERAL_ENG_THROTTLE_LEVER_POSITION:1", 100)
 time.sleep(0.1)
 aq.set("GENERAL_ENG_THROTTLE_LEVER_POSITION:2", 100)
 time.sleep(0.1)
 
-while n1 < 83:
+while n1 < 84.9:
     n1 = float(aq.get("TURB_ENG_N1:1"))
     
 end_time = time.time() - start_time
